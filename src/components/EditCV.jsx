84f68e7eb9d0a0faa476f89ef5fedb7629509e6d
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2022-03-07 16:25:26 +0200

    labels and more cohesion

--- src/components/EditCV.jsx
+++ src/components/EditCV.jsx
@@ -37,7 +37,8 @@ const EditCV = (props) => {
       type: "School level",
       title: "Education title",
       desc: "Description",
-      date: { start: null, end: null },
+      start: null,
+      end: null,
     };
     const educationEditing = [...education];
     educationEditing.push(defEdu);
@@ -64,92 +65,122 @@ const EditCV = (props) => {
       name: "Company name",
       jobTitle: "Title of your position.",
       jobTask: "Description of your tasks.",
-      date: { start: null, end: null },
+      start: null,
+      end: null,
     };
     const jobEditing = [...jobExperience];
     jobEditing.push(defJob);
     return setJobExperience(jobEditing);
   }
 
+  const loc = {
+    name: "Name",
+    fullName: "Fullname",
+    email: "Email Address",
+    phoneNumber: "Phone Number",
+    type: "School Level",
+    title: "Education Title",
+    jobTitle: "Job Title",
+    jobTask: "Task Description",
+    desc: "Description",
+    start: "Year started",
+    end: "Year ended",
+  };
+
   return (
     <div className="editCV">
-      <div className="editBasicInfo">
+      <div className="edit BasicInfo">
         {Object.entries(basicInfo).map((info) => {
           const key = info[0];
           const value = info[1];
+          if (key === "index") return;
           if (key !== "desc")
             return (
-              <input
-                type={typeof value === "string" ? "text" : "number"}
-                className={key}
-                value={value}
-                onChange={(e) => editBasicInfo({ [key]: e.target.value })}
-              />
+              <div className="inputWrapper">
+                <label htmlFor={`basic${key}`}>{loc[key]}</label>
+                <input
+                  type={typeof value === "string" ? "text" : "number"}
+                  className={key}
+                  id={`basic${key}`}
+                  value={value}
+                  onChange={(e) => editBasicInfo({ [key]: e.target.value })}
+                />
+              </div>
             );
           else
             return (
-              <textarea
-                type="text"
-                className={key}
-                value={value}
-                onChange={(e) => editBasicInfo({ [key]: e.target.value })}
-              />
+              <div className="textWrapper">
+                <label htmlFor={`basic${key}`}>{loc[key]}</label>
+                <textarea
+                  type="text"
+                  className={key}
+                  id={`basic${key}`}
+                  value={value}
+                  onChange={(e) => editBasicInfo({ [key]: e.target.value })}
+                />
+              </div>
             );
         })}
       </div>
-      <div className="editEducation">
+      <div className="edit Education">
         <button className="createNewEducation" onClick={addEducation}>
           Add Education
         </button>
         {education.map((edu, index) => {
           return (
-            <div className="eduTemplate">
+            <div className="edu Template">
               {Object.entries(edu).map((slot) => {
                 const key = slot[0];
                 const value = slot[1];
+                if (key === "index") return;
                 return (
-                  <input
-                    type={typeof value === "string" ? "text" : "number"}
-                    className={`edu${key}`}
-                    value={value}
-                    onChange={(e) =>
-                      editEducation({ [key]: e.target.value, index: index })
-                    }
-                  />
+                  <div className="inputWrapper">
+                    <label htmlFor={`edu${key}`}>{loc[key]}</label>
+                    <input
+                      type={typeof value === "string" ? "text" : "number"}
+                      className={`edu${key}`}
+                      id={`edu${key}`}
+                      value={value}
+                      onChange={(e) =>
+                        editEducation({ [key]: e.target.value, index: index })
+                      }
+                    />
+                  </div>
                 );
               })}
-              <button
-                className="removeEdu"
-                onClick={() => removeEducation(index)}
-              >
+              <button className="remove" onClick={() => removeEducation(index)}>
                 Remove
               </button>
             </div>
           );
         })}
       </div>
-      <div className="editJob">
+      <div className="edit Job">
         <button className="createNewJob" onClick={addJob}>
           Add Job Experience
         </button>
         {jobExperience.map((job, index) => {
           return (
-            <div className="jobTemplate">
+            <div className="job Template">
               {Object.entries(job).map((slot) => {
                 const key = slot[0];
                 const value = slot[1];
                 return (
-                  <input
-                    type={typeof value === "string" ? "text" : "number"}
-                    className={`job${key}`}
-                    value={value}
-                    onChange={(e) =>
-                      editJob({ [key]: e.target.value, index: index })
-                    }
-                  />
+                  <div className="inputWrapper">
+                    <label htmlFor={`job${key}`}>{loc[key]}</label>
+                    <input
+                      type={typeof value === "string" ? "text" : "number"}
+                      className={`job${key}`}
+                      id={`job${key}`}
+                      value={value}
+                      onChange={(e) =>
+                        editJob({ [key]: e.target.value, index: index })
+                      }
+                    />
+                  </div>
                 );
               })}
-              <button className="removeJob" onClick={() => removeJob(index)}>
+              <button className="remove" onClick={() => removeJob(index)}>
                 Remove
               </button>
             </div>
